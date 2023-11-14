0bf741aa9b29c3bb96090b2de4a839f2863d2e57
Rayane Kanuka <rayane.kanuka-benmahmoud@le-campus-numerique.fr>
2023-11-14 09:48:08 +0100

    Ajout de la classe Player

--- src/model/player/BotPlayer.java
+++ src/model/player/BotPlayer.java
@@ -1,4 +1,7 @@
 package model.player;
 
 public class BotPlayer extends Player{
+    public BotPlayer(char symbole, String name) {
+        super(symbole, name);
+    }
 }
--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -1,5 +1,37 @@
 package model.player;
 
-public  class Player {
+import java.util.Scanner;
 
+public abstract class Player { //TODO retravailler la classe Player (ébauche)
+    private char symbole;
+    private String name;
+
+    public Player(char symbole, String name) {
+        this.symbole = symbole;
+        this.name = name;
+    }
+
+    public int[] makeMove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(name + ", entre ton choix de case (ligne et colonne) : ");
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        return new int[]{row, col};
+    }
+
+    public char getSymbole() {
+        return symbole;
+    }
+
+    public void setSymbole(char symbole) {
+        this.symbole = symbole;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
