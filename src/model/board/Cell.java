a21fda3adc8b342107fbbef36e68648658aad6be
Rayane Kanuka <rayane.kanuka-benmahmoud@le-campus-numerique.fr>
2023-11-14 10:16:42 +0100

    Ajout de Cell pour le Board

--- src/model/board/Cell.java
+++ src/model/board/Cell.java
@@ -1,9 +1,16 @@
 package model.board;
 
 public class Cell {
+    private char Symbole;
 
     public Cell() {
-
+        this.Symbole = 'O';
     }
 
-}
+    public char getSymbole() {
+        return Symbole;
+    }
+
+    public void setSymbole(char symbole) {
+        Symbole = symbole;
+    }
