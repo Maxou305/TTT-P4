5859cc25b53766ac4f93142a595d5ec0b9d276c4
JBSLOTWINSKI <jean-baptiste.slotwinski@le-campus-numerique.fr>
2023-11-14 15:03:03 +0100

    bMéthodes Diago

--- src/model/board/Cell.java
+++ src/model/board/Cell.java
@@ -1,9 +1,28 @@
 package model.board;
 
 public class Cell {
+    public int x;
+    public int y;
+    public char Symbole;
 
-    public Cell() {
-
+    @Override
+    public String toString() {
+        return "Cell{" +
+                "Symbole=" + Symbole +
+                '}';
     }
 
+    public Cell(int x, int y) {
+        this.Symbole = ' ';
+        this.x = x;
+        this.y = y;
+    }
+
+    public char getSymbole() {
+        return Symbole;
+    }
+
+    public void setSymbole(char symbole) {
+        Symbole = symbole;
+    }
 }
